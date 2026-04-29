1400dd96ec865251e4fe5dba2b47580f19b81cfc
vlamelaryoo <enzo.lamela@gmail.com>
2026-04-29 12:23:50 +0800

    Update Wordle.py
    Finish scoring function, add test cases to test_game

--- Wordle.py
+++ Wordle.py
@@ -15,11 +15,19 @@
 DEBUG = True
 
 # Application Functions
-# TODO: Score Guess Function
+# Score Guess Function
 def score_guess(guess_word, target_word):
     score = []
     for letter in target_word:
         score.append(0)
+    if guess_word == target_word:
+        score = [2, 2, 2, 2, 2]
+    else:
+        for letter in range(len(target_word)):
+            if guess_word[letter] == target_word[letter]:
+                score[letter] = 2
+            elif guess_word[letter] in target_word:
+                score[letter] = 1
     return(score)
 
 # TODO: Read File Into Word List Function
@@ -51,6 +59,28 @@ def test_game():
 
     ## Assert
     print('Score:', score, 'Expected:', [0, 0, 0, 0, 0])
+
+    #Test Case 2
+    ## Arrange
+    guess_word = 'hello'
+    target_word = 'hello'
+
+    ## Act
+    score = score_guess(guess_word, target_word)
+
+    ## Assert
+    print('Score:', score, 'Expected:', [2, 2, 2, 2, 2])
+
+    #Test Case 3
+    ## Arrange
+    guess_word = 'world'
+    target_word = 'hello'
+
+    ## Act
+    score = score_guess(guess_word, target_word)
+
+    ## Assert
+    print('Score:', score, 'Expected:', [0, 1, 0, 2, 0])
 
 #TODO: Main Program
 if DEBUG == True:
