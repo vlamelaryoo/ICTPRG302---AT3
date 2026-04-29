ac44e1e8951e5f32f0ab5ae522b722a091460c54
vlamelaryoo <enzo.lamela@gmail.com>
2026-04-29 10:09:16 +0800

    Update AT3 Wordle Project.py
    Update project file to fit assessment formatting

--- AT3 Wordle Project.py
+++ AT3 Wordle Project.py
@@ -1,32 +1,35 @@
-#Vincent La Mela-Ryoo - 20169610
-#ICTPRG302 - AT3 Project - Wordle Game
+# AT3 - Wordle
+#
+# Author: Vincent La Mela-Ryoo
+# Student ID: 20169610
+#
+# Course: ICTPRG302
+# Lecturer: Maryam Shahabi
 
-#Welcom the user
-print('Welcome to Wordle!\n')
+# TODO: Add Import statements (if needed)
 
-#Define rules to be called and displayed when needed
-def rules():
-    print('The game rules are:')
-    print('Guess the randomly chosen 5 letter word.')
-    print('You will have 6 attempts.')
-    print('Your guesses must be 5 letters long and must be a valid word.')
-    print('Invalid guesses will not consume a guess attempt.')
-    print('After a guess, your word and each letter in it will scored:')
-    print(' - If a letter is in the word and in the correct posotion, a * will be displayed.')
-    print(' - If a letter is in the word but is in the incorrect position, a + will be displayed.')
-    print(' - If a letter is not in the word, a - will be displayed.')
-    print('If you guess the chosen word, you win!')
-    print('If you run out of guesses, you lose.\n')
+# Variables and Constants
+# TODO: Define Constants
 
-#Show rules before starting game
-rules()
+# TODO: Define Variables
 
-#import random function
-import random
-#open target_words file, read it and use splitlines to create list without newlines
-target_words = open('target_words.txt', 'r')
-target_words = target_words.read().splitlines()
-#use random function to choose a random word from target_words list, then capitalise it
-chosen_word = random.choice(target_words).upper()
-#test chosen_word working as intended
-print(str(chosen_word))
+# Application Functions
+# TODO: Score Guess Function
+
+# TODO: Read File Into Word List Function
+
+# TODO: Display Greeting Function
+def show_greeting():
+    print("Welcome")
+
+# TODO: Display Instructions Function
+def show_instructions():
+    print("Instructions")
+
+# TODO: Any Optional Additional Functions 
+
+# TODO: Play Game Function
+
+#TODO: Testing Function
+
+#TODO: Main Program
